7c0f43e4803c193d117018311ea2495b908dac03
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2026-09-16 17:51:24 +0200

    Add --all flag with argparse, env credentials and getpass

--- main.py
+++ main.py
@@ -1,27 +1,79 @@
-import sys, unicodedata
+import argparse
+import getpass
+import os
+import sys
+import unicodedata
+
 from hubyoung_lib import HubYoung
 
-print("*" * 10, "HubYoung downloader by @vvettoretti", "*" * 10)
 
-try:
-    username = sys.argv[1]
-    password = sys.argv[2]
-except:
-    username = input("Enter email\n")
-    password = input("Enter password\n")
-h = HubYoung(username, password)
+def safe_filename(title: str) -> str:
+    slug = unicodedata.normalize(
+        "NFD", title.lower().replace(" ", "_")
+    ).encode("ascii", "ignore").decode("ascii")
+    return slug
 
-print("Logged in successfully")
 
-library = h.get_library()
-books_to_download = []
-for book in library:
-    print(book["title"])
-    print("ID -", book["id"])
-    if input("Download? (y/n)\n").lower() == "y":
-        books_to_download.append(book)
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(
+        description="Download books from HUBScuola (Mondadori) and save them as PDFs."
+    )
+    parser.add_argument(
+        "username",
+        nargs="?",
+        default=os.environ.get("HUB_USER", ""),
+        help="HUBScuola email (or set HUB_USER env var)",
+    )
+    parser.add_argument(
+        "password",
+        nargs="?",
+        default=os.environ.get("HUB_PASS", ""),
+        help="HUBScuola password (or set HUB_PASS env var)",
+    )
+    parser.add_argument(
+        "--all",
+        dest="download_all",
+        action="store_true",
+        help="download all books without prompting",
+    )
+    return parser.parse_args(argv)
 
-for book in books_to_download:
-    print(f"Downloading {book['title']}... (this may take a while)")
-    book["title"] =  unicodedata.normalize('NFD', book["title"].lower().replace(' ', '_')).encode('ascii', 'ignore')
-    h.download_book(str(book["id"]), str(book["id"]) + "_" + book["title"].decode("ascii") + ".pdf")
+
+def main(argv=None) -> int:
+    print("*" * 10, "HubYoung downloader by @vvettoretti", "*" * 10)
+    args = parse_args(argv)
+
+    username = args.username or input("Enter email\n")
+    password = args.password or getpass.getpass("Enter password\n")
+
+    h = HubYoung(username, password)
+
+    print("Logged in successfully")
+
+    library = h.get_library()
+    if not library:
+        print("No books found in your library.")
+        return 0
+
+    books_to_download = []
+    if args.download_all:
+        books_to_download = list(library)
+        print(f"Downloading all {len(books_to_download)} book(s) (--all given)")
+    else:
+        for book in library:
+            print(book["title"])
+            print("ID -", book["id"])
+            if input("Download? (y/n)\n").lower() == "y":
+                books_to_download.append(book)
+
+    for book in books_to_download:
+        output_name = f"{book['id']}_{safe_filename(book['title'])}.pdf"
+        print(f"Downloading {book['title']}... (this may take a while)")
+        h.download_book(str(book["id"]), output_name)
+        print(f"Saved {output_name}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
